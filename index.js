b3b7ed3cfa642c707c227490a405e4ed66600065
ludde016 <ludde.swala@gmail.com>
2020-02-03 10:52:50 +0100

    Alot of more shit going on

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var clientDir = __dirname + '\\client\\'
+
+app.get('/', (req, res) => {
+  res.sendFile(clientDir + 'index.html')
+})
 app.listen(port, () => console.log(`Example app listening on port port!`))

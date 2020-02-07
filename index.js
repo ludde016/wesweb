9e792843472e8de190ec12135adaea809c3c8a6b
ludde016 <ludde.swala@gmail.com>
2020-02-07 13:53:02 +0100

    Good shit

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ var clientDir = __dirname + '\\client\\'
 app.get('/', (req, res) => {
   res.sendFile(clientDir + 'index.html')
 })
+app.get('/indexCSS.css', (req, res) => {
+  res.sendFile(clientDir + 'indexCSS.css')
+})
 app.listen(port, () => console.log(`Example app listening on port port!`))

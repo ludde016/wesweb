0d9dcd738f8fae0f799b9a2a8ad2c39ac0bd46b9
ludde016 <ludde.swala@gmail.com>
2020-02-17 10:26:33 +0100

    Form fixat

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 var clientDir = __dirname + '\\client\\'
 
 app.get('/', (req, res) => {
@@ -9,5 +11,14 @@ app.get('/', (req, res) => {
 })
 app.get('/indexCSS.css', (req, res) => {
   res.sendFile(clientDir + 'indexCSS.css')
+})
+
+app.get('/bajs', (req, res) => {
+  res.sendFile(clientDir+'Karl-bertil.jpg')
+})
+
+app.post('/form', (req, res) => {
+  const name = req.body.name
+  console.log(name)
 })
 app.listen(port, () => console.log(`Example app listening on port port!`))
